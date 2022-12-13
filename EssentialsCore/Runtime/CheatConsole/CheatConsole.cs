07b25485a12ffbff27e20ebaac0c795937fb1573
NotRewd <itsrewd@gmail.com>
2022-12-13 17:28:56 +0100

    Revert "Update CheatConsole.cs"
    This reverts commit 517d33d7ae34bfbeb35319a18d42b9522cc328e9.

--- EssentialsCore/Runtime/CheatConsole/CheatConsole.cs
+++ EssentialsCore/Runtime/CheatConsole/CheatConsole.cs
@@ -22,10 +22,6 @@ namespace Essentials.Core.CheatConsole
         public string title = "Cheat Console";
         [SerializeField] private Color color = new Color(0.1f, 0.1f, 0.1f, 0.5f);
         public Padding padding;
-        
-        [Header("Keybinds")]
-        [SerializeField] private InputActionReference openAction;
-        [SerializeField] private InputActionAsset[] inputsToDisable;
 
         [Header("Default Commands")]
         [SerializeField] private bool helpCommand = true;
@@ -44,8 +40,6 @@ namespace Essentials.Core.CheatConsole
 
         private Texture2D backgroundTexture;
         private Texture2D foregroundTexture;
-
-        private InputAction toggleAction;
 
         [Serializable]
         public struct Padding
@@ -124,19 +118,6 @@ namespace Essentials.Core.CheatConsole
         {
             backgroundTexture = EssentialsUtility.SingleTexture2DColor(color);
             foregroundTexture = EssentialsUtility.SingleTexture2DColor(new Color(0.8f, 0.8f, 0.8f, 0.5f));
-
-            if (openAction != null) toggleAction = openAction.action;
-            else
-            {
-                InputActionAsset asset = ScriptableObject.CreateInstance<InputActionAsset>();
-                InputActionMap map = asset.AddActionMap("Default");
-                
-                map.AddAction("Toggle", binding: "<Keyboard>/backquote");
-
-                toggleAction = asset.FindAction("Toggle");
-            }
-            
-            toggleAction.Enable();
 
             if (helpCommand) FindCommand("help").onExecute.AddListener(HelpCommand);
             if (printCommand) FindCommand("print").onExecuteRaw.AddListener(PrintCommand);
@@ -317,21 +298,17 @@ namespace Essentials.Core.CheatConsole
         
         private void Update()
         {
-            if (!toggleAction.WasPressedThisFrame()) return;
-            
-            if (!consoleVisible)
+            if (Keyboard.current.backquoteKey.wasPressedThisFrame)
             {
-                previousCursorLockMode = Cursor.lockState;
-                Cursor.lockState = CursorLockMode.None;
-                foreach (InputActionAsset asset in inputsToDisable) asset.Disable();
-            }
-            else
-            {
-                Cursor.lockState = previousCursorLockMode;
-                foreach (InputActionAsset asset in inputsToDisable) asset.Enable();
-            }
+                if (!consoleVisible)
+                {
+                    previousCursorLockMode = Cursor.lockState;
+                    Cursor.lockState = CursorLockMode.None;
+                }
+                else Cursor.lockState = previousCursorLockMode;
                 
-            consoleVisible = !consoleVisible;
+                consoleVisible = !consoleVisible;
+            }
         }
 
         #region Default Commands
