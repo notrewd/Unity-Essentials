2af7a9f76df4adda16ec8e4d5c3905bd949bf2e6
NotRewd <48103943+NotRewd@users.noreply.github.com>
2023-05-10 21:27:44 +0200

    Added support for adding new PlayerPrefs, editing, and deleting existing PlayerPrefs from the editor.

---
+++ EssentialsCore/Editor/PlayerPrefsEditor/PlayerPrefsAddEditor.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerPrefsAddEditor : EditorWindow
+{
+    private TextField keyField;
+    private TextField valueField;
+    private DropdownField typeField;
+    private Button addButton;
+
+    private PlayerPrefsEditorEditor editor;
+
+    public static void ShowWindow(PlayerPrefsEditorEditor editor)
+    {
+        PlayerPrefsAddEditor window = GetWindow<PlayerPrefsAddEditor>();
+        window.titleContent = new GUIContent("Add PlayerPrefs");
+        window.minSize = new Vector2(350, 150);
+
+        window.editor = editor;
+    }
+
+    public void CreateGUI()
+    {
+        VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/com.notrewd.essentials/EssentialsCore/Editor/PlayerPrefsEditor/PlayerPrefsAddEditorDocument.uxml");
+        visualTree.CloneTree(rootVisualElement);
+
+        keyField = rootVisualElement.Q<TextField>("KeyField");
+        valueField = rootVisualElement.Q<TextField>("ValueField");
+        typeField = rootVisualElement.Q<DropdownField>("TypeField");
+        addButton = rootVisualElement.Q<Button>("AddButton");
+
+        keyField.RegisterValueChangedCallback((_) => CheckFields());
+
+        valueField.RegisterValueChangedCallback((_) => CheckFields());
+
+        typeField.choices = new List<string>() { "String", "Int", "Float" };
+        typeField.value = "String";
+        typeField.RegisterValueChangedCallback((_) => CheckFields());
+
+        addButton.clicked += Add;
+    }
+
+    private void Add()
+    {
+        if (string.IsNullOrEmpty(keyField.value))
+        {
+            EditorUtility.DisplayDialog("Error", "Key cannot be empty", "OK");
+            return;
+        }
+
+        if (PlayerPrefs.HasKey(keyField.value))
+        {
+            EditorUtility.DisplayDialog("Error", "Key already exists", "OK");
+            return;
+        }
+
+        switch (typeField.value)
+        {
+            case "String":
+                editor.AddPlayerPref(keyField.value, valueField.value);
+                break;
+            case "Int":
+                if (!int.TryParse(valueField.value, out int intValue))
+                {
+                    EditorUtility.DisplayDialog("Error", "Value is not a valid integer", "OK");
+                    return;
+                }
+                editor.AddPlayerPref(keyField.value, Int16.Parse(valueField.value));
+                break;
+            case "Float":
+                if (!float.TryParse(valueField.value, out float floatValue))
+                {
+                    EditorUtility.DisplayDialog("Error", "Value is not a valid float", "OK");
+                    return;
+                }
+                editor.AddPlayerPref(keyField.value, double.Parse(valueField.value));
+                break;
+        }
+
+        Close();
+    }
+
+    private void CheckFields()
+    {
+        addButton.SetEnabled(false);
+
+        if (string.IsNullOrEmpty(keyField.value)) return;
+        else if (typeField.value == "Int" && !int.TryParse(valueField.value, out int intValue)) return;
+        else if (typeField.value == "Float" && !float.TryParse(valueField.value, out float floatValue)) return;
+
+        addButton.SetEnabled(true);
+    }
+}
--- EssentialsCore/Editor/PlayerPrefsEditor/PlayerPrefsEditorEditor.cs
+++ EssentialsCore/Editor/PlayerPrefsEditor/PlayerPrefsEditorEditor.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization.Plists;
 using UnityEditor;
 using UnityEngine;
@@ -9,9 +11,14 @@ public class PlayerPrefsEditorEditor : EditorWindow
 {
     private ScrollView playerPrefsList;
     private VisualElement noPlayerPrefsInfo;
+    private Button refreshButton;
     private VisualElement bottomBar;
-    private Button addKeyButton;
+    private Toggle internalPlayerPrefsToggle;
+    private Button newPlayerPrefButton;
     private Button applyButton;
+
+    private IDictionary playerPrefs;
+    private List<string> keysToDelete = new List<string>();
 
     [MenuItem("Essentials/PlayerPrefs Editor")]
     private static void ShowWindow()
@@ -28,38 +35,170 @@ public class PlayerPrefsEditorEditor : EditorWindow
 
         playerPrefsList = rootVisualElement.Q<ScrollView>("PlayerPrefsList");
         noPlayerPrefsInfo = rootVisualElement.Q<VisualElement>("NoPlayerPrefsInfo");
+        refreshButton = noPlayerPrefsInfo.Q<Button>("RefreshButton");
         bottomBar = rootVisualElement.Q<VisualElement>("BottomBar");
-        addKeyButton = bottomBar.Q<Button>("AddKeyButton");
+        internalPlayerPrefsToggle = bottomBar.Q<Toggle>("InternalPlayerPrefsToggle");
+        newPlayerPrefButton = bottomBar.Q<Button>("NewPlayerPrefButton");
         applyButton = bottomBar.Q<Button>("ApplyButton");
+
+        refreshButton.clicked += Reload;
+
+        internalPlayerPrefsToggle.RegisterValueChangedCallback((_) => RefreshList());
+
+        newPlayerPrefButton.clicked += () => PlayerPrefsAddEditor.ShowWindow(this);
+        applyButton.clicked += Apply;
 
         applyButton.SetEnabled(false);
 
+        Reload();
+    }
+
+    public void AddPlayerPref(string key, object value)
+    {
+        if (playerPrefs.Contains(key))
+        {
+            EditorUtility.DisplayDialog("Error", "Key already exists", "OK");
+            return;
+        }
+
+        playerPrefs.Add(key, value);
+
+        RefreshList();
+        applyButton.SetEnabled(true);
+    }
+
+    private void Reload()
+    {
+        LoadPlayerPrefs();
+        RefreshList();
+    }
+
+    private void LoadPlayerPrefs()
+    {
 #if PLATFORM_STANDALONE_OSX
         BinaryPlistReader reader = new BinaryPlistReader();
-        IDictionary dict = reader.ReadObject("/Users/" + Environment.UserName + "/Library/Preferences/unity." + PlayerSettings.companyName + "." + PlayerSettings.productName + ".plist");
+        playerPrefs = reader.ReadObject("/Users/" + Environment.UserName + "/Library/Preferences/unity." + PlayerSettings.companyName + "." + PlayerSettings.productName + ".plist");
+#endif
+    }
 
-        if (dict.Count == 0) return;
+    private void RefreshList()
+    {
+#if PLATFORM_STANDALONE_OSX
+        playerPrefsList.Clear();
 
-        noPlayerPrefsInfo.style.display = DisplayStyle.None;
-        playerPrefsList.style.display = DisplayStyle.Flex;
-
-        foreach (DictionaryEntry pair in dict)
+        foreach (DictionaryEntry pair in playerPrefs)
         {
+            if (!internalPlayerPrefsToggle.value && (pair.Key.ToString().StartsWith("unity.") || pair.Key.ToString() == "UnityGraphicsQuality")) continue;
+
             VisualElement element = new VisualElement();
             element.style.flexDirection = FlexDirection.Row;
 
-            Label keyLabel = new Label(pair.Key.ToString());
-            keyLabel.style.unityTextAlign = TextAnchor.MiddleCenter;
-
             TextField valueField = new TextField();
+            valueField.label = pair.Key.ToString();
             valueField.value = pair.Value.ToString();
+            valueField.style.flexShrink = 1;
             valueField.style.flexGrow = 1;
-            valueField.RegisterValueChangedCallback((_) => applyButton.SetEnabled(true));
 
-            element.Add(keyLabel);
+            DropdownField typeField = new DropdownField();
+            typeField.style.width = 100;
+            typeField.choices = new List<string>() { "String", "Int", "Float" };
+
+            if (pair.Value is string) typeField.value = "String";
+            else if (pair.Value is Int16) typeField.value = "Int";
+            else if (pair.Value is double) typeField.value = "Float";
+
+            valueField.RegisterValueChangedCallback((evt) =>
+            {
+                applyButton.SetEnabled(true);
+
+                if (typeField.value == "Int")
+                {
+                    if (!int.TryParse(evt.newValue, out int value)) applyButton.SetEnabled(false);
+                }
+                else if (typeField.value == "Float")
+                {
+                    if (!float.TryParse(evt.newValue, out float value)) applyButton.SetEnabled(false);
+                }
+            });
+
+            valueField.RegisterCallback<FocusOutEvent>((_) =>
+            {
+                if (typeField.value == "Int")
+                {
+                    if (!int.TryParse(valueField.value, out int value)) return;
+                    playerPrefs[pair.Key] = Int16.Parse(valueField.value);
+                }
+                else if (typeField.value == "Float")
+                {
+                    if (!float.TryParse(valueField.value, out float value)) return;
+                    playerPrefs[pair.Key] = double.Parse(valueField.value);
+                }
+                else playerPrefs[pair.Key] = valueField.value;
+            });
+
+            typeField.RegisterValueChangedCallback((evt) =>
+            {
+                if (evt.newValue == "Int")
+                {
+                    if (!int.TryParse(valueField.value, out int value)) valueField.value = "0";
+                    playerPrefs[pair.Key] = Int16.Parse(valueField.value);
+                }
+                else if (evt.newValue == "Float")
+                {
+                    if (!float.TryParse(valueField.value, out float value)) valueField.value = "0";
+                    playerPrefs[pair.Key] = double.Parse(valueField.value);
+                }
+                else playerPrefs[pair.Key] = valueField.value;
+
+                applyButton.SetEnabled(true);
+            });
+
+            ContextualMenuManipulator menuManipulator = new ContextualMenuManipulator((evt) =>
+            {
+                evt.menu.AppendAction("Delete PlayerPref", (_) =>
+                {
+                    keysToDelete.Add(pair.Key.ToString());
+                    playerPrefs.Remove(pair.Key);
+
+                    RefreshList();
+
+                    applyButton.SetEnabled(true);
+                });
+            });
+
+            element.AddManipulator(menuManipulator);
+
             element.Add(valueField);
+            element.Add(typeField);
             playerPrefsList.Add(element);
         }
 #endif
+
+        if (playerPrefsList.childCount > 0)
+        {
+            noPlayerPrefsInfo.style.display = DisplayStyle.None;
+            playerPrefsList.style.display = DisplayStyle.Flex;
+        }
+        else
+        {
+            noPlayerPrefsInfo.style.display = DisplayStyle.Flex;
+            playerPrefsList.style.display = DisplayStyle.None;
+        }
+    }
+
+    private void Apply()
+    {
+        PlayerPrefs.DeleteAll();
+
+        foreach (DictionaryEntry pair in playerPrefs)
+        {
+            if (pair.Value is string) PlayerPrefs.SetString(pair.Key.ToString(), pair.Value.ToString());
+            else if (pair.Value is Int16) PlayerPrefs.SetInt(pair.Key.ToString(), Int16.Parse(pair.Value.ToString()));
+            else if (pair.Value is double) PlayerPrefs.SetFloat(pair.Key.ToString(), float.Parse(pair.Value.ToString()));
+        }
+
+        PlayerPrefs.Save();
+
+        applyButton.SetEnabled(false);
     }
 }
