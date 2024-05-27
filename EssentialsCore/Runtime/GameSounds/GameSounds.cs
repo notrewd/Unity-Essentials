2f69fdcca54e7813943732f5f61d9e281dc63b2a
NotRewd <48103943+NotRewd@users.noreply.github.com>
2024-05-27 21:32:02 +0200

    Revert "Essentials GameSounds: Added the ability to change the audio output mixer group directly when creating a GameSound."
    This reverts commit 8fcca60c59c9049faa32e7534e17baeaa6a38fd8.

--- EssentialsCore/Runtime/GameSounds/GameSounds.cs
+++ EssentialsCore/Runtime/GameSounds/GameSounds.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Audio;
 using Object = UnityEngine.Object;
 
 namespace Essentials.Core.GameSounds
@@ -17,19 +16,18 @@ namespace Essentials.Core.GameSounds
             public AudioSource audioSource { get; }
 
             private readonly GameObject _gameObject;
-
-            public GameSound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, Vector3 position = default, Transform parent = null, bool looping = false, float volume = 1, float spatialBlend = 0)
+        
+            public GameSound(string id, AudioClip audioClip, Vector3 position = default, Transform parent = null, bool looping = false, float volume = 1, float spatialBlend = 0)
             {
                 this.id = id;
-
+                
                 _gameObject = new GameObject($"GameSound [{id}]");
                 if (parent != null) _gameObject.transform.SetParent(parent, false);
                 _gameObject.transform.localPosition = position;
-
+            
                 audioSource = _gameObject.AddComponent<AudioSource>();
                 audioSource.playOnAwake = false;
                 audioSource.clip = audioClip;
-                audioSource.outputAudioMixerGroup = audioMixerGroup;
                 audioSource.loop = looping;
                 audioSource.volume = volume;
                 audioSource.spatialBlend = spatialBlend;
@@ -37,14 +35,13 @@ namespace Essentials.Core.GameSounds
             }
 
             public void Destroy() => Object.Destroy(_gameObject);
-            public void DestroyImmediate() => Object.DestroyImmediate(_gameObject);
         }
-
+        
         private static readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private static CancellationToken _cancellationToken;
-
+        
         private static readonly HashSet<GameSound> _gameSounds = new HashSet<GameSound>();
-
+        
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Initialize()
         {
@@ -52,55 +49,40 @@ namespace Essentials.Core.GameSounds
             Application.quitting += OnApplicationQuit;
         }
 
-        public static AudioSource PlaySound(AudioClip audioClip, AudioMixerGroup audioMixerGroup = null, Transform parent = null, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, audioMixerGroup, default, parent, false, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, AudioMixerGroup audioMixerGroup, Transform parent, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, audioMixerGroup, default, parent, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, AudioMixerGroup audioMixerGroup, Vector3 position, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, audioMixerGroup, position, null, false, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, AudioMixerGroup audioMixerGroup, Vector3 position, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, audioMixerGroup, position, parent, false, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, AudioMixerGroup audioMixerGroup, Vector3 position, bool looping = false, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, audioMixerGroup, position, null, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, AudioMixerGroup audioMixerGroup, float volume, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, audioMixerGroup, default, null, false, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, AudioMixerGroup audioMixerGroup, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, audioMixerGroup, default, null, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, audioMixerGroup, default, parent, false, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, Transform parent, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, audioMixerGroup, default, parent, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, Vector3 position) => PlaySound(id, audioClip, audioMixerGroup, position, null, false);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, Vector3 position, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, audioMixerGroup, position, null, false, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, Vector3 position, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, audioMixerGroup, position, parent, false, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, Vector3 position, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, audioMixerGroup, position, null, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, audioMixerGroup, default, null, false, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup, bool looping, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, audioMixerGroup, default, null, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, Transform parent = null, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, null, default, parent, false, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, Transform parent, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, null, default, parent, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, Vector3 position, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, null, position, null, false, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, Vector3 position, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, null, position, parent, false, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, Vector3 position, bool looping = false, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, null, position, null, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, float volume, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, null, default, null, false, volume, spatialBlend);
-        public static AudioSource PlaySound(AudioClip audioClip, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, null, default, null, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, null, default, parent, false, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, Transform parent, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, null, default, parent, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position) => PlaySound(id, audioClip, null, position, null, false);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, null, position, null, false, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, null, position, parent, false, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, null, position, null, looping, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, null, default, null, false, volume, spatialBlend);
-        public static AudioSource PlaySound(string id, AudioClip audioClip, bool looping, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, null, default, null, looping, volume, spatialBlend);
-
-        public static AudioSource PlaySound(string id, AudioClip audioClip, AudioMixerGroup audioMixerGroup = null, Vector3 position = default, Transform parent = null, bool looping = false, float volume = 1, float spatialBlend = 0)
+        public static AudioSource PlaySound(AudioClip audioClip, Transform parent = null, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, default, parent, false, volume, spatialBlend);
+        public static AudioSource PlaySound(AudioClip audioClip, Transform parent, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, default, parent, looping, volume, spatialBlend);
+        public static AudioSource PlaySound(AudioClip audioClip, Vector3 position, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, position, null, false, volume, spatialBlend);
+        public static AudioSource PlaySound(AudioClip audioClip, Vector3 position, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, position, parent, false, volume, spatialBlend);
+        public static AudioSource PlaySound(AudioClip audioClip, Vector3 position, bool looping = false, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, position, null, looping, volume, spatialBlend);
+        public static AudioSource PlaySound(AudioClip audioClip, float volume, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, default, null, false, volume, spatialBlend);
+        public static AudioSource PlaySound(AudioClip audioClip, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound("Essentials.NoId", audioClip, default, null, looping, volume, spatialBlend);
+        public static AudioSource PlaySound(string id, AudioClip audioClip, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, default, parent, false, volume, spatialBlend);
+        public static AudioSource PlaySound(string id, AudioClip audioClip, Transform parent, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, default, parent, looping, volume, spatialBlend);
+        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position) => PlaySound(id, audioClip, position, null, false);
+        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, position, null, false, volume, spatialBlend);
+        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position, Transform parent, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, position, parent, false, volume, spatialBlend);
+        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position, bool looping, float volume = 1, float spatialBlend = 0) => PlaySound(id, audioClip, position, null, looping, volume, spatialBlend);
+        public static AudioSource PlaySound(string id, AudioClip audioClip, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, default, null, false, volume, spatialBlend);
+        public static AudioSource PlaySound(string id, AudioClip audioClip, bool looping, float volume, float spatialBlend = 0) => PlaySound(id, audioClip, default, null, looping, volume, spatialBlend);
+        
+        public static AudioSource PlaySound(string id, AudioClip audioClip, Vector3 position = default, Transform parent = null, bool looping = false, float volume = 1, float spatialBlend = 0)
         {
-            GameSound gameSound = new GameSound(id, audioClip, audioMixerGroup, position, parent, looping, volume, spatialBlend);
+            GameSound gameSound = new GameSound(id, audioClip, position, parent, looping, volume, spatialBlend);
             _gameSounds.Add(gameSound);
-
+            
             if (!looping) StopSoundAfter(gameSound, audioClip.length, _cancellationToken);
-
+            
             return gameSound.audioSource;
         }
-
+        
         public static void StopSound(string id)
         {
             GameSound[] gameSounds = _gameSounds.Where(gameSound => gameSound.id == id).ToArray();
-
+            
             foreach (GameSound gameSound in gameSounds) gameSound.Destroy();
             _gameSounds.RemoveWhere(gameSound => gameSound.id == id);
         }
-
+        
         public static void StopAllSounds()
         {
             foreach (GameSound gameSound in _gameSounds) gameSound.Destroy();
@@ -116,7 +98,7 @@ namespace Essentials.Core.GameSounds
 
             return false;
         }
-
+        
         private static async void StopSoundAfter(GameSound gameSound, float seconds, CancellationToken cancellationToken)
         {
             try
@@ -125,17 +107,15 @@ namespace Essentials.Core.GameSounds
             }
             catch (TaskCanceledException)
             {
-                gameSound.DestroyImmediate();
-                _gameSounds.Remove(gameSound);
                 return;
             }
-
+            
             if (!_gameSounds.Contains(gameSound)) return;
-
+            
             gameSound.Destroy();
             _gameSounds.Remove(gameSound);
         }
-
+        
         private static void OnApplicationQuit() => _cancellationTokenSource.Cancel();
     }
 }
