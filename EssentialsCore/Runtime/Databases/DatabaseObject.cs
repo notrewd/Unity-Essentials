5e336adbe86ddd41ebc70eec47ca9dfd74780e6f
NotRewd <48103943+NotRewd@users.noreply.github.com>
2024-07-23 16:27:59 +0200

    Added generic methods for retrieving items from a database

--- EssentialsCore/Runtime/Databases/DatabaseObject.cs
+++ EssentialsCore/Runtime/Databases/DatabaseObject.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Essentials.Core.Databases
@@ -8,6 +9,17 @@ namespace Essentials.Core.Databases
         public List<DatabaseItem> items = new List<DatabaseItem>();
 
         public DatabaseItem[] GetAllItems() => items.ToArray();
+
+        public T[] GetAllItems<T>() where T : DatabaseItem
+        {
+            return items.Cast<T>().ToArray();
+        }
+
         public DatabaseItem GetItem(string id) => items.Find(item => item.id == id);
+
+        public T GetItem<T>(string id) where T : DatabaseItem
+        {
+            return items.Find(item => item.id == id) as T;
+        }
     }
 }
